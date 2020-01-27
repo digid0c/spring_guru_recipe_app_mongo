79358a6755f2d4e87b79de8d820d071f05ea6b23
digid0c <digidoc22@gmail.com>
2020-01-27 22:44:24 +0200

    Create UnitOfMeasure entity, associate it with Ingredient

--- src/main/java/guru/samples/recipe/domain/Ingredient.java
+++ src/main/java/guru/samples/recipe/domain/Ingredient.java
@@ -13,6 +13,9 @@ public class Ingredient {
     private String description;
     private BigDecimal amount;
 
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure unitOfMeasure;
+
     @ManyToOne
     private Recipe recipe;
 
@@ -38,6 +41,14 @@ public class Ingredient {
 
     public void setAmount(BigDecimal amount) {
         this.amount = amount;
+    }
+
+    public UnitOfMeasure getUnitOfMeasure() {
+        return unitOfMeasure;
+    }
+
+    public void setUnitOfMeasure(UnitOfMeasure unitOfMeasure) {
+        this.unitOfMeasure = unitOfMeasure;
     }
 
     public Recipe getRecipe() {
