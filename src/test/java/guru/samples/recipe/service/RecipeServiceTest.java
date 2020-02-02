c82f534deea27a296e3aa1e2bb1393f3b1afec92
digid0c <digidoc22@gmail.com>
2020-02-02 16:42:54 +0200

    Implement RecipeService test using Mockito

---
+++ src/test/java/guru/samples/recipe/service/RecipeServiceTest.java
@@ -0,0 +1,41 @@
+package guru.samples.recipe.service;
+
+import guru.samples.recipe.domain.Recipe;
+import guru.samples.recipe.repository.RecipeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.*;
+
+public class RecipeServiceTest {
+
+    private RecipeService tested;
+
+    @Mock
+    private RecipeRepository recipeRepository;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        tested = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    public void shouldGetAllRecipes() {
+        Set<Recipe> recipes = Stream.of(new Recipe(), new Recipe(), new Recipe()).collect(Collectors.toSet());
+        when(recipeRepository.findAll()).thenReturn(recipes);
+
+        Set<Recipe> allRecipes = tested.findAll();
+        verify(recipeRepository, times(1)).findAll();
+        assertThat(allRecipes.size(), is(equalTo(recipes.size())));
+    }
+}
