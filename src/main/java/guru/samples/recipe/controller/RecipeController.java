22621171b275be7c3bbb35e0f098b4421a05e7ce
digid0c <digidoc22@gmail.com>
2020-02-10 06:47:18 +0200

    Implement feature to create a new recipe

--- src/main/java/guru/samples/recipe/controller/RecipeController.java
+++ src/main/java/guru/samples/recipe/controller/RecipeController.java
@@ -1,11 +1,11 @@
 package guru.samples.recipe.controller;
 
 import guru.samples.recipe.service.RecipeService;
+import guru.samples.recipe.view.RecipeView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/recipe")
@@ -22,5 +22,17 @@ public class RecipeController {
     public String getRecipe(@PathVariable Long id, Model model) {
         model.addAttribute("recipe", recipeService.findById(id));
         return "recipe/details";
+    }
+
+    @GetMapping("/new")
+    public String createNewRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeView());
+        return "recipe/recipe-form";
+    }
+
+    @PostMapping("/save")
+    public String saveOrUpdate(@ModelAttribute RecipeView recipe) {
+        RecipeView savedRecipe = recipeService.save(recipe);
+        return "redirect:/recipe/details/" + savedRecipe.getId();
     }
 }
