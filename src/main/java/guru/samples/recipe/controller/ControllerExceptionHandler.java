2d676474852e2eff09aa929ed9e3ff12754727c4
digid0c <digidoc22@gmail.com>
2020-03-08 19:23:17 +0200

    Refactor @ControllerAdvice based exception handler

--- src/main/java/guru/samples/recipe/controller/ControllerExceptionHandler.java
+++ src/main/java/guru/samples/recipe/controller/ControllerExceptionHandler.java
@@ -1,19 +1,23 @@
 package guru.samples.recipe.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.support.WebExchangeBindException;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 @Slf4j
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
-//    @ExceptionHandler(NumberFormatException.class)
-//    @ResponseStatus(BAD_REQUEST)
-//    public ModelAndView handleNumberFormatException(Exception exception) {
-//        log.error(exception.getMessage(), exception);
-//
-//        ModelAndView modelAndView = new ModelAndView("error-400");
-//        modelAndView.addObject("exception", exception);
-//        return modelAndView;
-//    }
+    @ExceptionHandler(WebExchangeBindException.class)
+    @ResponseStatus(BAD_REQUEST)
+    public String handleWebExchangeBindException(Exception exception, Model model) {
+        log.error(exception.getMessage(), exception);
+        model.addAttribute("exception", exception);
+        return "error-400";
+    }
 }
